c4e04f6957094c301ec3f502b11115af447d2c46
Cleber <cleber_batista2010@hotmail.com>
2022-12-01 22:50:41 -0300

    Tipos de declarar variaveis em Go

--- 2 - Variaveis/variaveis.go
+++ 2 - Variaveis/variaveis.go
@@ -9,4 +9,23 @@ func main() {
 	fmt.Println(variavel1)
 	fmt.Println(variavel2)
 
+	var (
+		variavel3 string = "variavel3"
+		variavel4 string = "variavel4"
+	)
+
+	fmt.Println(variavel3)
+	fmt.Println(variavel4)
+
+	variavel5, variavel6 := "Variavel5", "Variavel6"
+
+	fmt.Println(variavel5, variavel6)
+
+	const constan1 string = "Constant 1"
+	fmt.Println(constan1)
+
+	variavel5, variavel6 = variavel6, variavel5
+
+	fmt.Println(variavel5, variavel6)
+
 }
